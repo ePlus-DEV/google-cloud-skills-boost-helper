68db5f2725640ba0abceeff69f734dd49e8291c4
David Nguyen <h250694@gmail.com>
2025-03-11 17:11:34 +0700

    Add localization support for options page and leaderboard feature in English, French, and Vietnamese

--- popup.tsx
+++ popup.tsx
@@ -2,33 +2,24 @@ import { useStorage } from "@plasmohq/storage/hook"
 import "./style.css"
 
 function IndexPopup() {
-  const [openCount] = useStorage<number>("open-count", (storedCount) =>
-    typeof storedCount === "undefined" ? 0 : storedCount + 1
-  )
 
   const [checked, setChecked] = useStorage("checked", true)
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16,
-        width: 300,
-      }}>
-        <h1 className="text-2xl font-bold text-center">Cloud Skills Boost</h1>
-      <p className="text-gray-600 mt-2 text-center">Enhance your cloud skills with our leaderboard feature.</p>
+    <div style={{ display: "flex", flexDirection: "column", padding: 16, width: 300, }}>
+        <h3 className="text-xl font-semibold text-center">{ chrome.i18n.getMessage("extensionName") }</h3>
+        <p className="text-gray-600 mt-2 text-center">{ chrome.i18n.getMessage("optionsPageDescription") }</p>
       <div className="mt-4">
         <table className="min-w-full bg-white">
           <thead>
             <tr>
-              <th className="py-2">Tính năng</th>
-              <th className="py-2">Trạng thái</th>
+              <th className="py-2">{ chrome.i18n.getMessage("labelFeature") }</th>
+              <th className="py-2">{ chrome.i18n.getMessage("labelStatus") }</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="border px-4 py-2">Show Leaderboard</td>
+              <td className="border px-4 py-2">{ chrome.i18n.getMessage("labelShowLeaderboard") }</td>
               <td className="border px-4 py-2">
                 <input
                     type="checkbox"
