4ec2f8edaa0068d898f340167eac38cdaa5e2a2e
David Nguyen <h250694@gmail.com>
2025-03-11 18:08:59 +0700

    Update content component to change match URL and add leaderboard toggle functionality

--- content.tsx
+++ content.tsx
@@ -1,41 +1,54 @@
-import type { PlasmoCSConfig } from "plasmo"
-
-import { useStorage } from "@plasmohq/storage/hook"
+import type { PlasmoCSConfig } from "plasmo";
+import { useStorage } from "@plasmohq/storage/hook";
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.plasmo.com/*"]
-}
-
-// Idea for an UI API, for popup, notification badge, or mounting UI
-// Idea for static mount
-// Idea for styling injection support (inline or with custom emotion cache)
-
-export const getRootContainer = () => {
-  return document.querySelector("#feature")
-}
+  matches: ["https://www.cloudskillsboost.google/games/*/labs/*"]
+};
 
 const PlasmoOverlay = () => {
-  const [openCount] = useStorage<number>("open-count")
-  const [checked] = useStorage<boolean>("checked")
-  const [serialNumber] = useStorage<string>("serial-number")
+  const [checked] = useStorage<boolean>("checked");
+
+  const showScore = document.querySelector('.games-labs');
+  const leaderboard = document.querySelector('.js-lab-leaderboard');
+  
+  if (showScore) {
+    showScore.className = "lab-show l-full no-nav application-new lab-show l-full no-nav";
+  }
+
+  if (leaderboard) {
+      leaderboard.classList.add('hidden');
+  }
+
+  const toggleLeaderboard = () => {
+    
+   
+    if (leaderboard) {
+      leaderboard.classList.toggle('hidden');
+    }
+
+    console.log("Toggling leaderboard visibility");
+  };
 
   return (
-    <span
-      style={{
-        padding: 12
-      }}>
-      <h1>HELLO WORLD ROOT CONTAINER</h1>
-      <input
-        type={"checkbox"}
-        readOnly
-        checked={checked === undefined ? true : checked}
-      />
-      <p>
-        Open: {openCount}
-        <i>#{serialNumber}</i>
-      </p>
-    </span>
-  )
-}
+    <>
+      <button
+        onClick={toggleLeaderboard}
+        style={{
+          position: "fixed",
+          right: 0,
+          bottom: 5,
+          zIndex: 50,
+          backgroundColor: "#38a169",
+          color: "white",
+          padding: "0.5rem 1rem",
+          borderRadius: "0.25rem"
+        }}
+      >
+        Toggle Leaderboard
+      </button>
 
-export default PlasmoOverlay
+    </>
+  );
+};
+
+export default PlasmoOverlay;
