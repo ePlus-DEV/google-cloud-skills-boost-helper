98c67e023bd903334879ed08d9b5decae6d416fe
David Nguyen <h250694@gmail.com>
2025-03-11 16:46:54 +0700

    Add background script for secure and base storage testing; refactor popup and content components to use new storage hooks

--- popup.tsx
+++ popup.tsx
@@ -1,38 +1,22 @@
-import { useState, useEffect } from "react"
-import { Storage } from "@plasmohq/storage"
-const storage = new Storage({
-  copiedKeyList: ["shield-modulation"]
-})
-
+import { useStorage } from "@plasmohq/storage/hook"
 import "./style.css"
 
 function IndexPopup() {
-  const [showLeaderboard, setShowLeaderboard] = useState(false)
+  const [openCount] = useStorage<number>("open-count", (storedCount) =>
+    typeof storedCount === "undefined" ? 0 : storedCount + 1
+  )
 
-  useEffect(() => {
-    // Load the initial state from storage
-    storage.get("showLeaderboard").then((value) => {
-      if (value !== null) {
-        setShowLeaderboard(value === "true")
-      }
-    })
-
-    // Watch for changes in the storage
-    // storage.watch({
-    //   showLeaderboard: (c) => {
-    //     setShowLeaderboard(c.newValue === "true")
-    //   }
-    // })
-  }, [])
-
-  useEffect(() => {
-    // Save the state to storage whenever it changes
-    storage.set("showLeaderboard", showLeaderboard)
-  }, [showLeaderboard])
+  const [checked, setChecked] = useStorage("checked", true)
 
   return (
-    <div className="w-420px max-w-md mx-auto p-4 bg-white shadow-md rounded-lg">
-      <h1 className="text-2xl font-bold text-center">Cloud Skills Boost</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16,
+        width: 300,
+      }}>
+        <h1 className="text-2xl font-bold text-center">Cloud Skills Boost</h1>
       <p className="text-gray-600 mt-2 text-center">Enhance your cloud skills with our leaderboard feature.</p>
       <div className="mt-4">
         <table className="min-w-full bg-white">
@@ -47,10 +31,10 @@ function IndexPopup() {
               <td className="border px-4 py-2">Show Leaderboard</td>
               <td className="border px-4 py-2">
                 <input
-                  type="checkbox"
-                  checked={showLeaderboard}
-                  className="form-checkbox h-5 w-5 text-blue-600"
-                  onChange={() => setShowLeaderboard(!showLeaderboard)}
+                    type="checkbox"
+                    className="form-checkbox h-5 w-5 text-blue-600"
+                    checked={checked}
+                    onChange={(e) => setChecked(e.target.checked)}
                 />
               </td>
             </tr>
