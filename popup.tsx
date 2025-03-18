68ebd2bfde2bc0b95c869967a13f74f57ba7c1cb
David Nguyen <h250694@gmail.com>
2025-03-18 13:55:51 +0700

    Add rel attribute to external link for improved security

--- popup.tsx
+++ popup.tsx
@@ -55,7 +55,7 @@ function IndexPopup() {
             </div>
             <footer className="mt-4 text-center text-gray-500">
                 <FontAwesomeIcon icon={faCopyright} /> {new Date().getFullYear()} -{" "}
-                <a href="http://eplus.dev" target="_blank" className="text-blue-600">
+                <a href="http://eplus.dev" target="_blank" rel="noopener noreferrer" className="text-blue-600">
                     ePlus.DEV
                 </a>
             </footer>
