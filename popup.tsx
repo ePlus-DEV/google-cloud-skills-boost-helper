f10c2b9cec8cfc8afa59eba206977ca2290da877
David Nguyen <h250694@gmail.com>
2025-03-21 13:31:40 +0700

    Update ProfileCard import path and pass additional points props in IndexPopup

--- popup.tsx
+++ popup.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useStorage } from "@plasmohq/storage/hook";
 import BadgeTable from "~components/feature/badge";
-import ProfileCard from "~components/feature/profile";
+import ProfileCard from "~components/feature/arcade/profile";
 import "./style.css";
 
 function IndexPopup() {
@@ -19,8 +19,7 @@ function IndexPopup() {
 
     return (
         <div className="w-[400px] mx-auto grid grid-cols-1 md:grid-cols-4 gap-4 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900 shadow-lg rounded-lg p-1">
-            <ProfileCard userName={userName} league={league} ArcadePoints={totalPoints} points={points} lastUpdated={lastUpdated} />
-            <BadgeTable gamePoints={gamePoints} triviaPoints={triviaPoints} skillPoints={skillPoints} specialPoints={specialPoints} />
+            <ProfileCard userName={userName} league={league} ArcadePoints={totalPoints} points={points} lastUpdated={lastUpdated} gamePoints={gamePoints} triviaPoints={triviaPoints} skillPoints={skillPoints} specialPoints={specialPoints}/>
         </div>
     );
 }
