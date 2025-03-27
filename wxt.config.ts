73548fa70243c0b322f97b78582946debd681784
David Nguyen <h250694@gmail.com>
2025-03-27 16:19:13 +0700

    Refactor extension metadata, add localization support, and implement new components for options page

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    name: "__MSG_extName__",
+    description: "__MSG_extDescription__",
+    default_locale: "en",
     permissions: ["storage"],
   },
   extensionApi: "chrome",
