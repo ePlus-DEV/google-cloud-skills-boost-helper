fa3375d47ad2d15852f489bbaccb71e90bd235ea
David Nguyen <h250694@gmail.com>
2025-03-28 12:17:00 +0700

    Remove FontAwesome icon rendering from popup entry point and clean up unused imports

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,15 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCrown } from "@fortawesome/duotone-regular-svg-icons";
-import { createRoot } from "react-dom/client";
-
 document.title = browser.i18n.getMessage("optionsPageTitle");
 
 declare const messageH1: HTMLHeadingElement;
 messageH1.textContent = browser.i18n.getMessage("optionsPageTitle");
-
-// render fontawrsome icons vào id messageH2
-const messageH2 = document.getElementById("messageH2");
-if (messageH2) {
-	const root = createRoot(messageH2);
-	root.render(<FontAwesomeIcon icon={faCrown} />);
-}
