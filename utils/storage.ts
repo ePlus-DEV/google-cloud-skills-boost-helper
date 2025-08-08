c8a1941bbf0e86a5616bfd21282526fc4c66b24f
David Nguyen <h250694@gmail.com>
2025-08-08 09:25:08 +0700

    chore: correct storage item types

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,11 +1,13 @@
+import type { ArcadeData, BadgeData } from "../types";
+
 const leaderBoardChecked = storage.defineItem<boolean>("local:leaderBoard", {
   fallback: false,
 });
 
-const urlProfile = storage.defineItem<boolean>("local:urlProfile");
-const arcadeData = storage.defineItem<boolean>("local:arcadeData", {
-  fallback: false,
+const urlProfile = storage.defineItem<string>("local:urlProfile");
+const arcadeData = storage.defineItem<ArcadeData>("local:arcadeData", {
+  fallback: {},
 });
-const arcadeBadges = storage.defineItem<boolean>("local:arcadeBadges", {
-  fallback: false,
+const arcadeBadges = storage.defineItem<BadgeData[]>("local:arcadeBadges", {
+  fallback: [],
 });
