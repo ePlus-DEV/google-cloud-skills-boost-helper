be45bab6fa64d5a24c0c0a110a9da07491d73718
David Nguyen <h250694@gmail.com>
2026-07-28 21:05:23 +0700

    feat: initialize improved account creation UX

--- entrypoints/options/main.tsx
+++ entrypoints/options/main.tsx
@@ -5,6 +5,7 @@ import { initDataManagementToggle } from "./dataManagementToggle";
 import { initUIToggles } from "./uiToggles";
 import { initNicknamePreview } from "./nicknamePreview";
 import { initNotificationSettings } from "./notificationSettings";
+import { initAccountCreationUX } from "./accountCreationUX";
 
 // Set document title
 document.title =
@@ -69,6 +70,7 @@ async function initializeOptionsPage() {
   initUIToggles();
   initNicknamePreview();
   initNotificationSettings();
+  initAccountCreationUX();
 }
 
 initializeOptionsPage().catch((error) => {
